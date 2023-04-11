825a8590ef69fcdd7e39721b34afd4bd2be431eb
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2023-04-11 18:32:45 -0300

    Registro y mejoras esteticas

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: [''],
+      username: ['', [Validators.required]],
+      correo: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]],
+    });
+  }
+  registrarse() {
+    console.log(this.myForm.value);
   }
 
+  ngOnInit(): void {}
 }
