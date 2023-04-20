24a7126b3c1a9d3701be48c7ace5d05d82c4d9ab
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2023-04-20 09:40:30 -0300

    Invertidas Pages y Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
-import { CatalogoComponent } from './Components/catalogo/catalogo.component';
-import { RegistroComponent } from './Components/registro/registro.component';
-import { LoginComponent } from './Components/login/login.component';
-import { DetalleComponent } from './Components/detalle/detalle.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { CatalogoComponent } from './Pages/catalogo/catalogo.component';
+import { RegistroComponent } from './Pages/registro/registro.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { DetalleComponent } from './Pages/detalle/detalle.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // módulos para el cliente http y los formularios
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuComponent } from './Pages/menu/menu.component';
+import { MenuComponent } from './Components/menu/menu.component';
 import { PosterPipe } from './Pipes/poster.pipe';
 import { OverviewPipe } from './Pipes/overview.pipe';
-import { ListadoGeneroComponent } from './Components/listado-genero/listado-genero.component';
+import { ListadoGeneroComponent } from './Pages/listado-genero/listado-genero.component';
 
 @NgModule({
   declarations: [
